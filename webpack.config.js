d67fcf0d08abe9ae58328918253c71f6aea01aed
johel <johel.carvalho@gmail.com>
2016-06-05 12:02:20 -0300

    refreshing css

--- webpack.config.js
+++ webpack.config.js
@@ -37,14 +37,18 @@ var config;
 // Detect how npm is run and branch based on that
 switch(process.env.npm_lifecycle_event) {
   case 'build':
-    config = merge(common, {});
+    config = merge(common, parts.setupCSS(PATHS.app));
     break;
   default:
-    config = merge(common, parts.devServer({
+    config = merge(
+      common,
+      parts.devServer({
       // Customize host/port here if needed
         host: process.env.HOST,
         port: process.env.PORT
-    }));
+      }),
+      parts.setupCSS(PATHS.app)
+    );
 }
 
 module.exports = validate(config);
